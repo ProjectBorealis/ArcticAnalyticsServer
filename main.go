bb5c43fcde9807408dbb5de3a45494f2e8833c35
Arran Walker <arran.walker@fiveturns.org>
2018-10-08 12:49:32 +0000

    feat: server cli options
    - Default port can be changed (--port)
    - Specify whether we're behind a reverse proxy (--behind-proxy).
      When this is true, the X-Forwarded-For header is parsed to get
      get the client IP.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"os"
@@ -14,20 +15,34 @@ const (
 	sharedSecretEnv = "SHARED_SECRET"
 )
 
+var (
+	behindProxy = flag.Bool("behind-proxy", false, "whether server is running behind a reverse proxy.")
+	httpPort    = flag.Int("port", 9095, "http server port")
+)
+
 func main() {
+	flag.Parse()
+
 	rw, err := server.NewResultWriter("results.json")
 	if err != nil {
 		panic(err)
 	}
 	defer rw.Close()
 
-	s := server.New(rw, os.Getenv(sharedSecretEnv))
-	loggedRouter := handlers.LoggingHandler(os.Stdout, s.Handler())
+	handler := server.New(rw, os.Getenv(sharedSecretEnv)).Handler()
+
+	// enable proxy middleware if behind a proxy
+	if *behindProxy {
+		handler = handlers.ProxyHeaders(handler)
+	}
+
+	// enable logging
+	handler = handlers.LoggingHandler(os.Stdout, handler)
 
 	srv := &http.Server{
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
-		Handler:      loggedRouter,
+		Handler:      handler,
 		Addr:         ":9095",
 	}
 	log.Println(srv.ListenAndServe())
